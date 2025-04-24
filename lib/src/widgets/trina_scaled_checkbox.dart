cac808c5a11644abab183885cb2e92bafba27ac6
Feras abdalrahman <doonfrs@gmail.com>
2025-04-24 05:01:16 +0300

    Fix checkbox theme reset bug

--- lib/src/widgets/trina_scaled_checkbox.dart
+++ lib/src/widgets/trina_scaled_checkbox.dart
@@ -33,18 +33,13 @@ class TrinaScaledCheckbox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Transform.scale(
       scale: scale,
-      child: Theme(
-        data: ThemeData(
-          unselectedWidgetColor: unselectedColor,
-        ),
-        child: Checkbox(
-          value: value,
-          tristate: tristate,
-          onChanged: handleOnChanged,
-          activeColor: value == null ? unselectedColor : activeColor,
-          checkColor: checkColor,
-          side: side,
-        ),
+      child: Checkbox(
+        value: value,
+        tristate: tristate,
+        onChanged: handleOnChanged,
+        activeColor: value == null ? unselectedColor : activeColor,
+        checkColor: checkColor,
+        side: side,
       ),
     );
   }
