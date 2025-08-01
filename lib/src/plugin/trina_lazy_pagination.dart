f0e5bc32bb52106df87bf64afa791e2bdc979588
Mouayad <muayad.perun@outlook.com>
2025-08-01 08:56:03 +0300

    feat: allow custom pagination UI
    This adds a `builder` property to `TrinaLazyPagination`
     which allows users to customize the rows pagination UI.

--- lib/src/plugin/trina_lazy_pagination.dart
+++ lib/src/plugin/trina_lazy_pagination.dart
@@ -8,6 +8,13 @@ import 'package:trina_grid/trina_grid.dart';
 typedef TrinaLazyPaginationFetch = Future<TrinaLazyPaginationResponse> Function(
     TrinaLazyPaginationRequest);
 
+/// Callback function to build a custom pagination widget.
+///
+typedef TrinaPaginationBuilder = Widget Function(
+  BuildContext context,
+  TrinaLazyPaginationState state,
+);
+
 /// Request data for lazy pagination processing.
 class TrinaLazyPaginationRequest {
   TrinaLazyPaginationRequest({
@@ -137,6 +144,7 @@ class TrinaLazyPagination extends StatefulWidget {
     required this.fetch,
     required this.stateManager,
     this.onLazyFetchCompleted,
+    this.builder,
     super.key,
   }) : assert(
           !showPageSizeSelector || pageSizes.contains(initialPageSize),
@@ -197,6 +205,9 @@ class TrinaLazyPagination extends StatefulWidget {
   final void Function(int page, int totalPage, int? totalRecords)?
       onLazyFetchCompleted;
 
+  /// A callback function that returns a widget for custom pagination.
+  final TrinaPaginationBuilder? builder;
+
   @override
   State<TrinaLazyPagination> createState() => TrinaLazyPaginationState();
 }
@@ -217,6 +228,12 @@ class TrinaLazyPaginationState extends State<TrinaLazyPagination> {
   int get pageSize => _pageSize;
   int get totalPage => _totalPage;
   int? get totalRecords => _totalRecords;
+
+  bool get isFirstPage => _page < 2;
+
+  bool get isLastPage => _page >= _totalPage;
+
+  int get _pageSizeToMove => widget.pageSizeToMove ?? 1;
 
   @override
   void initState() {
@@ -319,8 +336,37 @@ class TrinaLazyPaginationState extends State<TrinaLazyPagination> {
     setPage(1);
   }
 
+  void firstPage() => setPage(1);
+
+  void lastPage() => setPage(_totalPage);
+
+  void previousPage() {
+    var beforePage = _page - _pageSizeToMove;
+    if (beforePage < 1) {
+      beforePage = 1;
+    }
+    setPage(beforePage);
+  }
+
+  void nextPage() {
+    var nextPage = _page + _pageSizeToMove;
+    if (nextPage > _totalPage) {
+      nextPage = _totalPage;
+    }
+    setPage(nextPage);
+  }
+
+  /// Triggers a re-fetch of the current page's data.
+  void refresh() {
+    setPage(_page);
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (widget.builder != null) {
+      return widget.builder!(context, this);
+    }
+
     return _PageSizeDropdownPaginationWidget(
       iconColor: stateManager.style.iconColor,
       disabledIconColor: stateManager.style.disabledIconColor,
